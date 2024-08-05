a75846f03fb8e1262f779c126d80a89d1760cb1a
kainat-ashfaq <kainat.ashfaq135@gmail.com>
2024-08-06 02:24:13 +0500

    word-scramble-game

--- src/script.js
+++ src/script.js
@@ -3,33 +3,39 @@ var check = document.getElementById("check-button");
 var input = document.getElementById("input");
 var hint = document.getElementById("hint-div");
 var time = document.getElementById("time-div");
+var timer = document.getElementById("time");
+
 var un_scrambled_word = document.getElementById("unscrambled-word");
-var seconds = 30
+var seconds = 30;
 var interval;
+var correct_word = []
 
 
-
-
-check.addEventListener("click",function(){
+//  var currentWordIndex = -1;
+check.addEventListener("click", function () {
   var input_value = input.value.toLowerCase();
   for (let j = 0; j < technology_words.length; j++) {
 
-    if(technology_words[j].toLocaleLowerCase() == input_value){
+    if (input_value === technology_words[j].toLowerCase()) {
       hint.textContent = "Correct!";
-      un_scrambled_word.innerText = technology_words[j];
-      input.value = ""
+      un_scrambled_word.textContent = technology_words[j];
+      clearInterval(interval);
+
+      input.vale = ""
       break;
-    }else{
+    } else {
       hint.textContent = "Incorrect!";
-      input.value = ""
-      break;
+      input.vale = ""
+
+
     }
+
   }
-})
+});
 
 
-var  technology_words = [
-"interface",
+var technology_words = [
+  "interface",
   "Computer",
   "Internet",
   "Database",
@@ -81,32 +87,34 @@ var  technology_words = [
   "Password",
 ];
 
+refresh2.addEventListener("click", function () {
+  seconds = 30
+  // clearInterval(interval);
+      interval = setInterval(() => {
+     seconds--
+      if (seconds === 0 ) {
+        clearInterval(interval)
+        hint.innerText = "Time's up!";
+        input.value = "";
+        time.style.color = "#413748";
 
 
+      }
+      if(seconds == 10){
+        time.style.color = "red"
 
+      }
+  time.innerHTML = `Time Left: ${seconds} seconds`
+     
+    },1000)
 
+  var number = Math.floor(Math.random() * scrambled_words.length);
+  un_scrambled_word.innerText = scrambled_words[number].word;
+   
 
-refresh2.addEventListener("click",function(){
-  // seconds = ""
-    interval = setInterval(() => {
-   seconds--
-    if (seconds === 0 ) {
-      clearInterval(interval)
-      hint.innerText = "Time's up!";
-      input.value = "";
-      
-    }
-time.innerHTML = `Time Left: ${seconds} seconds`
-
-  },1000)
-
-  var number = Math.floor(Math.random()*scrambled_words.length)
-un_scrambled_word.innerText = scrambled_words[number].word
-hint.innerText = `Hint :${scrambled_words[number].hint}`;
+  hint.innerText = `Hint :${scrambled_words[currentWordIndex].hint}`;
   input.value = "";
-
-})
-
+});
 
 var scrambled_words = [
   {
@@ -121,11 +129,26 @@ var scrambled_words = [
     word: "a e d t b a s a",
     hint: "An organized collection of structured information",
   },
-  { word: "e n h t r e e t", hint: "A common method of networking computers in a LAN" },
-  { word: "w m r i e f r a", hint: "Software that is embedded in a hardware device" },
-  { word: "w l f r i a e l", hint: "A system designed to prevent unauthorized access" },
-  { word: "g b y i g a e t", hint: "A unit of data storage equal to 1,024 megabytes" },
-  { word: "h d a r e w r a", hint: "The physical components of a computer system" },
+  {
+    word: "e n h t r e e t",
+    hint: "A common method of networking computers in a LAN",
+  },
+  {
+    word: "w m r i e f r a",
+    hint: "Software that is embedded in a hardware device",
+  },
+  {
+    word: "w l f r i a e l",
+    hint: "A system designed to prevent unauthorized access",
+  },
+  {
+    word: "g b y i g a e t",
+    hint: "A unit of data storage equal to 1,024 megabytes",
+  },
+  {
+    word: "h d a r e w r a",
+    hint: "The physical components of a computer system",
+  },
   {
     word: "t f s o e w a r",
     hint: "The programs and other operating information used by a computer",
@@ -138,11 +161,26 @@ var scrambled_words = [
     word: "p o c r e s o r s",
     hint: "The part of a computer that performs calculations",
   },
-  { word: "t o l c o r p t o", hint: "A set of rules governing data communication" },
-  { word: "t y i r c u e s", hint: "Measures taken to protect a computer or network" },
-  { word: "e s e s i w r l", hint: "Using radio waves to transmit data without cables" },
-  { word: "h o t o t l u e b", hint: "Wireless technology standard for exchanging data" },
-  { word: "e g y b t a e m", hint: "A unit of data storage equal to 1,024 kilobytes" },
+  {
+    word: "t o l c o r p t o",
+    hint: "A set of rules governing data communication",
+  },
+  {
+    word: "t y i r c u e s",
+    hint: "Measures taken to protect a computer or network",
+  },
+  {
+    word: "e s e s i w r l",
+    hint: "Using radio waves to transmit data without cables",
+  },
+  {
+    word: "h o t o t l u e b",
+    hint: "Wireless technology standard for exchanging data",
+  },
+  {
+    word: "e g y b t a e m",
+    hint: "A unit of data storage equal to 1,024 kilobytes",
+  },
   { word: "e y k o a b r d", hint: "Input device with keys used to type text" },
   {
     word: "o r n t o m i",
@@ -174,12 +212,18 @@ var scrambled_words = [
     word: "s s w o r a p d",
     hint: "A string of characters used to gain access to a computer system",
   },
-  { word: "r l e p h e i a r p", hint: "External devices connected to a computer" },
+  {
+    word: "r l e p h e i a r p",
+    hint: "External devices connected to a computer",
+  },
   {
     word: "r m o u a s i l t",
     hint: "A program that imitates real-world processes or systems",
   },
-  { word: "t a r y e t e b", hint: "A unit of data storage equal to 1,024 gigabytes" },
+  {
+    word: "t a r y e t e b",
+    hint: "A unit of data storage equal to 1,024 gigabytes",
+  },
   {
     word: "m s e u n r a e",
     hint: "An identifier used by a person to gain access to a computer system",
@@ -188,7 +232,10 @@ var scrambled_words = [
     word: "i u a t r v l",
     hint: "Not physically existing but made to appear by software",
   },
-  { word: "m e w b t s a r e", hint: "A person responsible for maintaining a website" },
+  {
+    word: "m e w b t s a r e",
+    hint: "A person responsible for maintaining a website",
+  },
   {
     word: "m p t o e u r c",
     hint: "An electronic device for storing and processing data",
@@ -197,11 +244,26 @@ var scrambled_words = [
     word: "a e d t b a s a",
     hint: "An organized collection of structured information",
   },
-  { word: "n h t r e e t", hint: "A common method of networking computers in a LAN" },
-  { word: "w m r i e f r a", hint: "Software that is embedded in a hardware device" },
-  { word: "w l f r i e a r", hint: "A system designed to prevent unauthorized access" },
-  { word: "g b y i g a e t", hint: "A unit of data storage equal to 1,024 megabytes" },
-  { word: "h d a r e w r", hint: "The physical components of a computer system" },
+  {
+    word: "n h t r e e t",
+    hint: "A common method of networking computers in a LAN",
+  },
+  {
+    word: "w m r i e f r a",
+    hint: "Software that is embedded in a hardware device",
+  },
+  {
+    word: "w l f r i e a r",
+    hint: "A system designed to prevent unauthorized access",
+  },
+  {
+    word: "g b y i g a e t",
+    hint: "A unit of data storage equal to 1,024 megabytes",
+  },
+  {
+    word: "h d a r e w r",
+    hint: "The physical components of a computer system",
+  },
   {
     word: "t f s o e w a r",
     hint: "The programs and other operating information used by a computer",
@@ -210,10 +272,22 @@ var scrambled_words = [
     word: "t n r e i e t n",
     hint: "A global network connecting millions of computers",
   },
-  { word: "t i y r c u e s", hint: "Measures taken to protect a computer or network" },
-  { word: "e s i w r s e l", hint: "Using radio waves to transmit data without cables" },
-  { word: "h o t o t l u e b", hint: "Wireless technology standard for exchanging data" },
-  { word: "e g y b t a e m", hint: "A unit of data storage equal to 1,024 kilobytes" },
+  {
+    word: "t i y r c u e s",
+    hint: "Measures taken to protect a computer or network",
+  },
+  {
+    word: "e s i w r s e l",
+    hint: "Using radio waves to transmit data without cables",
+  },
+  {
+    word: "h o t o t l u e b",
+    hint: "Wireless technology standard for exchanging data",
+  },
+  {
+    word: "e g y b t a e m",
+    hint: "A unit of data storage equal to 1,024 kilobytes",
+  },
   { word: "e y k o a b r d", hint: "Input device with keys used to type text" },
   {
     word: "o r n t o m i",
@@ -225,4 +299,3 @@ var scrambled_words = [
     hint: "A string of characters used to gain access to a computer system",
   },
 ];
-
